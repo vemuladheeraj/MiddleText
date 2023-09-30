1fd2f88e9fe8fc4d936dc4197352286bf22b600e
Dheeraj Vemula <vemula.dheeraj@gmail.com>
2023-09-30 12:47:30 +0530

    Add project files.

---
+++ MiddleText/MainClass.cs
@@ -0,0 +1,44 @@
+﻿namespace MiddleText
+{
+    public class MainClass
+    {
+        /// <summary>
+        /// This will return the text between the startText and endText.
+        /// </summary>
+        /// <param name="enireText"></param>
+        /// <param name="startText"></param>
+        /// <param name="endText"></param>
+        /// <returns>String</returns>
+        public static string GetText(string enireText,string startText,string endText)
+        {
+            int startIndex = enireText.IndexOf(startText);
+            int endIndex = enireText.IndexOf(endText,startIndex);
+
+            if (startIndex != -1 && endIndex != -1)
+            {
+                return enireText.Substring(startIndex + startText.Length, endIndex - startIndex - startText.Length);               
+            }
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// This will return the text between the startText(Ignoring the characters based on skipStartIndex) and endText 
+        /// </summary>
+        /// <param name="enireText"></param>
+        /// <param name="startText"></param>
+        /// <param name="endText"></param>
+        /// <param name="skipStartIndex"></param>
+        /// <returns>String</returns>
+        public static string GetText(string enireText, string startText, string endText,int skipStartIndex)
+        {
+            int startIndex = enireText.IndexOf(startText,skipStartIndex);
+            int endIndex = enireText.IndexOf(endText, startIndex);
+
+            if (startIndex != -1 && endIndex != -1)
+            {
+                return enireText.Substring(startIndex + startText.Length, endIndex - startIndex - startText.Length);
+            }
+            return String.Empty;
+        }
+    }
+}
---
+++ MiddleText/Readme.md
@@ -0,0 +1,2 @@
+This will return the text between the startText and endText.
+This will return the text between the startText(Ignoring the characters based on skipStartIndex) and endText 
